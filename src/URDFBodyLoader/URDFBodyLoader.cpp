edc37c883dc90116729821f45a9a0ef37b0c0512
Yuki Onishi <yuki.st16@gmail.com>
2021-05-01 03:07:08 +0900

    Add a function finding the root link

--- src/URDFBodyLoader/URDFBodyLoader.cpp
+++ src/URDFBodyLoader/URDFBodyLoader.cpp
@@ -18,6 +18,14 @@ using namespace cnoid;
 
 namespace filesystem = cnoid::stdx::filesystem;
 using fmt::format;
+using pugi::xml_node;
+
+const char CHILD[] = "child";
+const char JOINT[] = "joint";
+const char LINK[] = "link";
+const char NAME[] = "name";
+const char PARENT[] = "parent";
+const char ROBOT[] = "robot";
 
 namespace {
 struct Registration
@@ -42,8 +50,16 @@ public:
 
     Impl();
     bool load(Body* body, const string& filename);
-};
 
+private:
+    vector<xml_node> findChildrenByGrandchildAttribute(
+        const xml_node& node,
+        const char* child_name,
+        const char* grandchild_name,
+        const char* attr_name,
+        const char* attr_value);
+    vector<xml_node> findRootLinks(const xml_node& robot);
+};
 }  // namespace cnoid
 
 URDFBodyLoader::URDFBodyLoader()
@@ -81,5 +97,58 @@ bool URDFBodyLoader::Impl::load(Body* body, const string& filename)
     pugi::xml_document doc;
     pugi::xml_parse_result result = doc.load_file(filename.c_str());
     os() << "Load result: " << result.description() << endl;
+
+    // checks if only one 'robot' tag exists in the URDF
+    if (++doc.children(ROBOT).begin() != doc.children(ROBOT).end()) {
+        os() << "Error: Multiple 'robot' tags are found.";
+        return false;
+    }
+
+    // parses the robot structure ('robot' tag)
+    xml_node robot = doc.child(ROBOT);
+
+    vector<xml_node> rootLinks = findRootLinks(robot);
+
+    if (rootLinks.size() != 1) {
+        os() << "Error: " << rootLinks.size() << " root links are found.";
+        return false;
+    }
+
     return true;
+}
+
+vector<xml_node> URDFBodyLoader::Impl::findChildrenByGrandchildAttribute(
+    const xml_node& node,
+    const char* child_name,
+    const char* grandchild_name,
+    const char* attr_name,
+    const char* attr_value)
+{
+    vector<xml_node> result;
+    for (xml_node child : node.children(child_name)) {
+        xml_node candidate = child.find_child_by_attribute(grandchild_name,
+                                                           attr_name,
+                                                           attr_value);
+        if (candidate != xml_node()) {
+            result.push_back(candidate);
+        }
+    }
+    return result;
+}
+
+vector<xml_node> URDFBodyLoader::Impl::findRootLinks(const xml_node& robot)
+{
+    vector<xml_node> rootLinks;
+    for (xml_node link : robot.children(LINK)) {
+        if (findChildrenByGrandchildAttribute(robot,
+                                              JOINT,
+                                              CHILD,
+                                              LINK,
+                                              link.attribute(NAME).value())
+                .size()
+            == 0) {
+            rootLinks.push_back(link);
+        }
+    }
+    return rootLinks;
 }
